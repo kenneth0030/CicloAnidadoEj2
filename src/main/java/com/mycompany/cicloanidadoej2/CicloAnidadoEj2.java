56a239c90d704719c89ce91354365771a71c6163
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:33:31 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/cicloanidadoej2/CicloAnidadoEj2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cicloanidadoej2;
+
+/**
+ *
+ * @author kenne
+ */
+public class CicloAnidadoEj2 {
+    
+  public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(i + "" + j);
+            }
+        }
+    }
+}
